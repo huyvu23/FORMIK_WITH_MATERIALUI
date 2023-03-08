65f27b9d061fd00a90951689a6a2c090ecc878f4
Huy Vũ <93518371+huyvu23@users.noreply.github.com>
2023-03-08 16:43:22 +0700

    [ADD] - first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Formik, Form } from "formik";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Grid, Typography } from "@material-ui/core";
+import * as Yup from "yup";
+
+import Select from "./components/FormUI/Select";
+import DateTimePicker from "./components/FormUI/DateTimePicker";
+import TextFieldWrapper from "./components/FormUI/TextField";
+import countries from "./data/countries.json";
+import Checkbox from "./components/FormUI/Checkbox";
+
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+const INITIAL_FORM_STATE = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  country: "",
+  arrivealDate: "",
+  departureDate: "",
+  message: "",
+  termsOfService: false,
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email.").required("Required"),
+  phone: Yup.number()
+    .integer()
+    .typeError("Please enter a valid phone number")
+    .required("Required"),
+  country: Yup.string().required("Required"),
+  arrivealDate: Yup.date().required("Required"),
+  departureDate: Yup.date().required("Required"),
+  message: Yup.string(),
+  termsOfService: Yup.boolean()
+    .oneOf([true], "The terms and conditions must be accepted.")
+    .required("The terms and conditions must be accepted."),
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item xs={12}></Grid>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+            <Formik
+              initialValues={{
+                ...INITIAL_FORM_STATE,
+              }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={(values) => {
+                console.log(values);
+              }}
+            >
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography>Your Detail</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextFieldWrapper name="firstName" label="First Name" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <TextFieldWrapper name="lastName" label="Last Name" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextFieldWrapper name="email" label="Email" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextFieldWrapper name="phone" label="Phone" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Select
+                      name="country"
+                      label="Country"
+                      options={countries}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <DateTimePicker name="arrivealDate" label="Arrival Date" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <DateTimePicker
+                      name="departureDate"
+                      label="Departure Date"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextFieldWrapper
+                      name="message"
+                      label="Message"
+                      multiline={true}
+                      rows={4}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Checkbox
+                      name="termsOfService"
+                      legend="Terms Of Service"
+                      label="I agree"
+                    />
+                  </Grid>
+                </Grid>
+              </Form>
+            </Formik>
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
   );
 }
 
